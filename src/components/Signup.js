9041dd2a3af52073544ad36259b8a945bab1ed76
unknown <you@example.com>
2021-11-02 22:40:29 -0400

    updated signup and styling a smidge,signup working

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React,{useState} from "react";
+import { NavLink,Redirect } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -7,31 +7,70 @@ import Card from "react-bootstrap/Card";
 import "../styles/Signup.css";
 
 const Signup = () => {
-    const _handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(e.target.username.value)
-        console.log(e.target.email.value)
-        console.log(e.target.password.value)
-    }
+    const [signedUp,setSignedUp] = useState(false)
+    const [message,setMessage] = useState('')
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      username: e.target.username.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    fetch("http://localhost:3001/api/signup", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data.message)
+        if(data.created){
+            setSignedUp(true);
+            localStorage.setItem('username',e.target.username.value)
+        }else{
+            setMessage(data.message)
+        }
+      })
+
+  };
+  if(signedUp){
+    return(
+        <Redirect to='/create'/>
+    )
+  }
   return (
     <div>
       <h1>Signup</h1>
       <div className="form">
         <Container>
-          <Card bg='info'>
+          <Card bg="info">
             <Form onSubmit={_handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicUsername">
-                <Form.Label className='label'>Username</Form.Label>
-                <Form.Control type="text" placeholder="Create a username" name='username' />
+                <Form.Label className="label">Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Create a username"
+                  name="username"
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label className='label'>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email"  name='email'/>
+                <Form.Label className="label">Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label className='label'>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" name='password'/>
+                <Form.Label className="label">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
               </Form.Group>
               <Button variant="light" type="submit">
                 Sign Up
@@ -39,6 +78,7 @@ const Signup = () => {
             </Form>
           </Card>
         </Container>
+        <p className='error'>{message}</p>
       </div>
       <NavLink to="/">Return Home</NavLink>
     </div>
