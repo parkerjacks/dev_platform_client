34e15bd71cd37c0fb08fbf21799e620c17c4e76a
unknown <you@example.com>
2021-11-09 20:06:46 -0500

    testing production signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -16,7 +16,7 @@ const Signup = () => {
       email: e.target.email.value,
       password: e.target.password.value,
     };
-    fetch("http://localhost:3001/user/signup", {
+    fetch("https://dev-platform-server-deployed.herokuapp.com/user/signup", {
       method: "POST", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
