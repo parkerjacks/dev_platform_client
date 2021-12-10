878e21e934dd0658289c3f278a5d361b6576e119
unknown <you@example.com>
2021-12-09 21:55:32 -0500

    resolved language selection minimum issue -tpannell

--- src/components/Createprofile.js
+++ src/components/Createprofile.js
@@ -3,17 +3,15 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Known from './Known'
-import {useState} from 'react'
+import Known from "./Known";
+import { useState } from "react";
 //import {Redirect} from 'react-router-dom'
 import { useHistory } from "react-router-dom";
 import Tolearn from "./Tolearn";
 
-
 const Createprofile = () => {
   const [created, setCreated] = useState(false);
   const [profilePic, setProfilePic] = useState(null);
-  const [message,setMessage] = useState('')
   const history = useHistory();
 
   //Converts User Img to URL and Store in State Variable
@@ -22,12 +20,12 @@ const Createprofile = () => {
     reader.addEventListener("load", () => {
       setProfilePic(reader.result);
       //localStorage.setItem("profile-pic", reader.result)
-    })
+    });
     reader.readAsDataURL(e.target.files[0]);
-  }
- 
+  };
+
   //console.log(profilePic);
-  
+
   const _handleSubmit = (e) => {
     e.preventDefault();
     //console.log(e);
@@ -42,9 +40,7 @@ const Createprofile = () => {
       ) {
         // console.log({name:knownCheckBoxes[i].name,message:'This is a known language'})
         knownChecked.push(knownCheckBoxes[i].name);
-        if(knownChecked.length > 2){
-          setMessage('Please limit your selections to 3 languages')
-        }
+        
       }
       if (
         knownCheckBoxes[i].checked &&
@@ -53,11 +49,9 @@ const Createprofile = () => {
       ) {
         // console.log({name:knownCheckBoxes[i].name,message:'This is a language to learn'})
         toLearnChecked.push(knownCheckBoxes[i].name);
-        if(toLearnChecked.length > 2){
-          setMessage('Please limit your selections to 3 languages')
-        }
       }
     }
+
 
     const data = {
       github: e.target.github.value,
@@ -65,35 +59,43 @@ const Createprofile = () => {
       portfolio: e.target.portfolio.value,
       knownLanguages: knownChecked,
       toLearn: toLearnChecked,
-      userImg: profilePic
+      userImg: profilePic,
     };
 
-    localStorage.setItem('profilePic', e.target.pic.value)
+    localStorage.setItem("profilePic", e.target.pic.value);
     //console.log(e.target.pic.value);
-    
-    fetch(`http://localhost:3001/user/${localStorage.getItem('username')}/profile/create`, {
-        method: "PUT", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data)
-          if(data.update){
+     if(knownChecked.length > 3 || toLearnChecked.length > 3){
+        alert('Please limit language selections to 3 or less.')
+      } else{
+
+        fetch(
+          `http://localhost:3001/user/${localStorage.getItem(
+            "username"
+          )}/profile/create`,
+          {
+            method: "PUT", // or 'PUT'
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+        
+            if (data.update) {
               //console.log('updated')
               setCreated(true);
-              console.log(created); //This rtns false ?? 
-              
+    
               let theUserId = data.userId;
               history.push(`/profile/${theUserId}`);
-          }
-        })
+            }
+          });
+      }
+    
   };
 
-
- /*  if(created){
+  /*  if(created){
       return(
           <Redirect to='/profile' />
       )
@@ -108,8 +110,8 @@ const Createprofile = () => {
       </p>
       <Container>
         <Card bg="info">
-            <p>Make sure to include https:// or http:// with links</p>
-          <Form onSubmit={_handleSubmit} encType='multipart/form-data'>
+          <p>Make sure to include https:// or http:// with links</p>
+          <Form onSubmit={_handleSubmit} encType="multipart/form-data">
             <Card bg="light" style={{ margin: "5px" }}>
               {/* github */}
               <p>Github:</p>
@@ -139,16 +141,19 @@ const Createprofile = () => {
                 />
               </Form.Group>
             </Card>
-
             {/* languages you know */}
-            <Known/>
+            <Known />
 
             {/* languages you want to learn */}
-            <Tolearn/>
+            <Tolearn />
             <Card style={{ margin: "5px" }} bg="light">
               <Form.Group>
                 <Form.Label>Upload Profile Picture:</Form.Label>
-                <Form.Control name='pic' type="file" onChange={_handlePicUpload} />
+                <Form.Control
+                  name="pic"
+                  type="file"
+                  onChange={_handlePicUpload}
+                />
               </Form.Group>
             </Card>
             <Button variant="light" type="submit">
