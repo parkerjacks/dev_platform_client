8eaec64344e309ab54abfef879ac01eb2fce223c
Stella <stelladev7@gmail.com>
2022-01-04 21:26:31 -0500

    adding chat functionality

--- src/components/chat_components/Chat.js
+++ src/components/chat_components/Chat.js
@@ -1,45 +1,48 @@
 import { Component } from "react";
-import Title from './Title'
+import Title from "./Title";
 import ChatFeed from "./Feed";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 import Form from "./Form";
 class Chat extends Component {
-    constructor(){
-        super()
-        this.state = {
-            currentUser:localStorage.getItem('currentUser'),
-            otherUser:localStorage.getItem('otherUser'),
-            messages:''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      currentUser: localStorage.getItem("currentUser"),
+      otherUser: localStorage.getItem("otherUser"),
+      messages: [],
+      chatId: "",
+    };
+  }
 
-    componentDidMount = () =>{
-        console.log('we are going to find this chat')
-        console.log(this.state.currentUser,this.state.otherUser)
-        fetch(`http://localhost:3001/chat/${this.state.currentUser}/${this.state.otherUser}`)
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data.message)
-        })
-    }
+  componentDidMount = () => {
+    console.log("we are going to find this chat");
+    console.log(this.state.currentUser, this.state.otherUser);
+    fetch(
+      `http://localhost:3001/chat/${this.state.currentUser}/${this.state.otherUser}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.chatId);
+        this.setState({ chatId: data.chatId });
+        fetch('http://localhost:3001/message/' + data.chatId)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.messages);
+            this.setState({messages: data.messages})
+          });
+      });
+  };
 
-
-    render() {
-
-        return (
-
-            <div>
-
-               <Title/> 
-                <ChatFeed/>
-                <Form/>
-                <NavLink to="/profile">Back to Profile</NavLink>
-                
-            </div>
-        )
-
-
-    }
+  render() {
+    return (
+      <div>
+        <Title />
+        <ChatFeed chatId={this.state.chatId} messages={this.state.messages} />
+        <Form chatId={this.state.chatId} />
+        <NavLink to="/profile">Back to Profile</NavLink>
+      </div>
+    );
+  }
 }
 
 export default Chat;
--- src/components/chat_components/Feed.js
+++ src/components/chat_components/Feed.js
@@ -1,13 +1,25 @@
-import { Component } from "react"; 
+import { Component } from "react";
 
-class ChatFeed extends Component { 
-    render(){ 
-        return( 
-            <div> 
-                <h1> Chat Feed </h1>
+class ChatFeed extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Chat Feed </h1>
+        {this.props.messages.map((message) => {
+          return (
+            <div key={message.id}>
+             <p>{message.message}</p>
             </div>
-        )
-    }
-} 
+          )
+        })}
+      </div>
+    );
+  }
+}
 
 export default ChatFeed;
