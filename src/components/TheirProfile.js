3c9e650048681ad375ddadfc236bf67a8c17639a
Park Jack <parker.jackson.hhs@gmail.com>
2022-01-08 13:55:06 -0500

    added html and css changes to feed component

---
+++ src/components/TheirProfile.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+class TheirProfile extends Component {
+    render() {
+
+        return (
+            <div> 
+                <div> Username: </div>
+                <div> Profile Picture: </div>
+                <div> Languages: </div>
+                <div> Chat: </div>
+
+            </div>
+
+        )
+
+    }
+}
+
+
+
+export default TheirProfile;
